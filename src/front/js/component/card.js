f177ef2f11e996fd0227164f724244f6960f55ea
Tgonz089 <tristangonzales8936@gmail.com>
2022-11-19 16:55:44 +0000

    added color to the types.

--- src/front/js/component/card.js
+++ src/front/js/component/card.js
@@ -3,8 +3,35 @@ import { useState, useEffect, useContext } from "react";
 import { Context } from "../store/appContext";
 import { Link, useParams } from "react-router-dom";
 
+const TYPE_COLORS = {
+  bug: "B1C12E",
+  dark: "4F3A2D",
+  dragon: "755EDF",
+  electric: "FCBC17",
+  fairy: "F4B1F4",
+  fighting: "823551D",
+  fire: "E73B0C",
+  flying: "A3B3F7",
+  ghost: "6060B2",
+  grass: "74C236",
+  ground: "D3B357",
+  ice: "A3E7FD",
+  normal: "C8C4BC",
+  poison: "934594",
+  psychic: "ED4882",
+  rock: "B9A156",
+  steel: "B5B5C3",
+  water: "3295F6",
+};
+
 export const PokemonCard = (props) => {
   const { store, actions } = useContext(Context);
+  const typing = props.type.split(",");
+
+  const data = typing.map(types => {
+    return types.trim()
+  })
+  console.log(data);
 
   return (
     <div className="card pokemon-card">
@@ -14,10 +41,25 @@ export const PokemonCard = (props) => {
         <p className="card-text">
           <strong>Number:</strong> {props.index + 1}
         </p>
-        <p className="card-text">
-          <strong>Type:</strong> {props.type}
+        <p className="card-text typing">
+        {data.map((type) => (
+          <span
+            key={type}
+            className="badge badge-pill mr-1"
+            style={{
+              backgroundColor: `#${TYPE_COLORS[type]}`,
+              color: "white",
+            }}
+          >
+            {type
+              .toLowerCase()
+              .split(" ")
+              .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+              .join(" ")}
+          </span>
+        ))}
         </p>
-        <Link to={"/pokemon/" + (props.index)} className="btn btn-danger">
+        <Link to={"/pokemon/" + props.index} className="btn btn-danger">
           Learn More
         </Link>
         <button
--- src/front/js/pages/pokemon.js
+++ src/front/js/pages/pokemon.js
@@ -3,6 +3,27 @@ import PropTypes from "prop-types";
 import { Link, useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
 
+const TYPE_COLORS = {
+  bug: "B1C12E",
+  dark: "4F3A2D",
+  dragon: "755EDF",
+  electric: "FCBC17",
+  fairy: "F4B1F4",
+  fighting: "823551D",
+  fire: "E73B0C",
+  flying: "A3B3F7",
+  ghost: "6060B2",
+  grass: "74C236",
+  ground: "D3B357",
+  ice: "A3E7FD",
+  normal: "C8C4BC",
+  poison: "934594",
+  psychic: "ED4882",
+  rock: "B9A156",
+  steel: "B5B5C3",
+  water: "3295F6",
+};
+
 export const Pokemon = (props) => {
   const { store, actions } = useContext(Context);
   const params = useParams();
@@ -12,13 +33,36 @@ export const Pokemon = (props) => {
   </style>
 
   const data = store.pokemon[params.id];
+
+  const typing = data.type.split(",");
+  const trimmedData = typing.map(types => {
+    return types.trim()
+  })
+  console.log(data);
+
   return (
     <div className="jumbotron">
       <div>
         <img className="soloimg" src={data.image} />
         <h1 className="display-4">{data.name}</h1>
-        <p className="single">
-          <strong>Type:</strong> {data.type}</p>
+        <p className="single typingPoke">
+        {trimmedData.map((type) => (
+          <span
+            key={type}
+            className="badge badge-pill mr-1"
+            style={{
+              backgroundColor: `#${TYPE_COLORS[type]}`,
+              color: "white",
+            }}
+          >
+            {type
+              .toLowerCase()
+              .split(" ")
+              .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+              .join(" ")}
+          </span>
+        ))}
+        </p>
         <p className="single"><strong>Number:</strong> {data.id}</p>
         <p className="single"><strong>Abilities:</strong> {data.abilities}</p>
         <p className="single"><strong>Stat Names:</strong> {data.stat_names}</p>
