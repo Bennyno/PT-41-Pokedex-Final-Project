798fe32f8e379e18c2ef404d8da2647d36981f50
Tgonz089 <tristangonzales8936@gmail.com>
2022-11-22 17:45:09 +0000

    fixed the login and sign up boxes.

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -18,37 +18,39 @@ export const Login = () => {
 
   return (
     <div className="container form">
-    <form className="box">
-    <div className="text-center mt-5">
-      <h1 className="header">Log In</h1>
-      <div className="text-center">
-        No registered yet?{" "}
-        <Link to="/signup">
-          <span className="signUpLink">Sign Up</span>
-        </Link>
+      <div className="text-center mt-5">
+        <h1 className="header">Log In</h1>
+        <div className="text-center">
+          Not registered yet?{" "}
+          <Link to="/signup">
+            <span className="signUpLink">Sign Up</span>
+          </Link>
+        </div>
+        <div>
+          <input
+            className="email"
+            type="text"
+            placeholder="Email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+          <input
+            className="password"
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+          <div>
+          <button className="btn btn-primary newsign" onClick={handleClick}>
+            Login
+          </button>
+          </div>
+          <p className="text-center mt-2">
+            Forgot <a href="#">password?</a>
+          </p>
+        </div>
       </div>
-      <div>
-        <input
-          className="email"
-          type="text"
-          placeholder="Email"
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
-        />
-        <input
-          className="password"
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-        />
-        <button className= "btn btn-primary newsign"  onClick={handleClick}>Login</button>
-        <p className="text-center mt-2">
-          Forgot <a href="#">password?</a>
-        </p>
-      </div>
-    </div>
-    </form>
     </div>
   );
 };
--- src/front/js/pages/signup.js
+++ src/front/js/pages/signup.js
@@ -9,53 +9,40 @@ export const SignUp = (props) => {
   const [password, setPassword] = useState("");
 
   return (
-    // <div className="text-center mt-5">
-    //   <h1 className="SIGNUP">Sign Up</h1>
-    //   <div>
-    //       <input
-    //         type="text"
-    //         placeholder="Email"
-    //         value={email}
-    //         onChange={(event) => setEmail(event.target.value)}
-    //       />
-    //       <input
-    //         type="password"
-    //         placeholder="Password"
-    //         value={password}
-    //         onChange={(event) => setPassword(event.target.value)}
-    //       />
-    //       <button onClick={()=>actions.signup(email,password)}>Sign Up</button>
-    //     </div>
-    // </div>
     <div className="container form">
-    <form className="box">
-    <div className="text-center mt-5">
-      <h1 className="header">Sign Up</h1>
-      <div className="text-center">
-        Already have an account?{" "}
-        <Link to="/login">
-          <span className="signUpLink">Log In</span>
-        </Link>
+      <div className="text-center mt-5">
+        <h1 className="SIGNUP">Sign Up</h1>
+        <div className="text-center">
+          Already have an account?{" "}
+          <Link to="/login">
+            <span className="signUpLink">Log In</span>
+          </Link>
+        </div>
+        <div>
+          <input
+            className="email"
+            type="text"
+            placeholder="Email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+          <input
+            className="password"
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+          <div>
+          <button
+            className="btn btn-primary signupbutton"
+            onClick={() => actions.signup(email, password)}
+          >
+            Sign Up
+          </button>
+          </div>
+        </div>
       </div>
-      <div>
-        <input
-          className="email"
-          type="text"
-          placeholder="Email"
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
-        />
-        <input
-          className="password"
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-        />
-        <button className= "btn btn-primary newsign" onClick={()=>actions.signup(email,password)}>Sign Up</button>
-      </div>
-    </div>
-    </form>
     </div>
   );
 };
