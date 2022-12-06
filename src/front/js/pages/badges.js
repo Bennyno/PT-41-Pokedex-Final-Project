eab18ca29c80acb5b86588bf722a5dbc16c18e49
Bennyno <ben.fernandez28@gmail.com>
2022-12-06 01:20:29 +0000

    Uploaded badge pics to img folder

--- src/front/js/pages/badges.js
+++ src/front/js/pages/badges.js
@@ -1,5 +1,63 @@
 import React, { useEffect, useContext, useState } from "react";
 import Boulder from "../../img/Boulder_Badge.jpg";
+import Cascade from "../../img/Cascade_Badge.png";
+import Thunder from "../../img/Thunder_Badge.png";
+import Rainbow from "../../img/Rainbow_Badge.png";
+import Soul from "../../img/Soul_Badge.png";
+import Marsh from "../../img/Marsh_Badge.png";
+import Volcano from "../../img/Volcano_Badge.png";
+import Earth from "../../img/Earth_Badge.png";
+import Zephyr from "../../img/Zephyr_Badge.png";
+import Hive from "../../img/Hive_Badge.png";
+import Plain from "../../img/Plain_Badge.png";
+import Fog from "../../img/Fog_Badge.png";
+import Storm from "../../img/Storm_Badge.png";
+import Mineral from "../../img/Mineral_Badge.png";
+import Glacier from "../../img/Glacier_Badge.png";
+import Rising from "../../img/Rising_Badge.png";
+import Stone from "../../img/Stone_Badge.png";
+import Knuckle from "../../img/Knuckle_Badge.png";
+import Dynamo from "../../img/Dynamo_Badge.png";
+import Heat from "../../img/Heat_Badge.png";
+import Balance from "../../img/Balance_Badge.png";
+import Feather from "../../img/Feather_Badge.png";
+import Mind from "../../img/Mind_Badge.png";
+import Rain from "../../img/Rain_Badge.png";
+import Coal from "../../img/Coal_Badge.png";
+import Forest from "../../img/Forest_Badge.png";
+import Cobble from "../../img/Cobble_Badge.png";
+import Fen from "../../img/Fen_Badge.png";
+import Relic from "../../img/Relic_Badge.png";
+import Mine from "../../img/Mine_Badge.png";
+import Icicle from "../../img/Icicle_Badge.png";
+import Beacon from "../../img/Beacon_Badge.png";
+import Trio from "../../img/Trio_Badge.png";
+import Basic from "../../img/Basic_Badge.png";
+import Insect from "../../img/Insect_Badge.png";
+import Bolt from "../../img/Bolt_Badge.png";
+import Quake from "../../img/Quake_Badge.png";
+import Jet from "../../img/Jet_Badge.png";
+import Freeze from "../../img/Freeze_Badge.png";
+import Legend from "../../img/Legend_Badge.png";
+import Bug from "../../img/Bug_Badge.png";
+import Cliff from "../../img/Cliff_Badge.png";
+import Rumble from "../../img/Rumble_Badge.png";
+import Plant from "../../img/Plant_Badge.png";
+import Voltage from "../../img/Voltage_Badge.png";
+import Fairy from "../../img/Fairy_Badge.png";
+import Psychic from "../../img/Psychic_Badge.png";
+import Iceberg from "../../img/Iceberg_Badge.png";
+import Grass from "../../img/Grass_Badge.png";
+import Water from "../../img/Water_Badge.png";
+import Fire from "../../img/Fire_Badge.png";
+import Fighting from "../../img/Fighting_Badge.png";
+import Ghost from "../../img/Ghost_Badge.png";
+import Galar_Fairy from "../../img/Galar_Fairy_Badge.png";
+import Rock from "../../img/Rock_Badge.png";
+import Ice from "../../img/Ice_Badge.png";
+import Dark from "../../img/Dark_Badge.png";
+import Dragon from "../../img/Dragon_Badge.png";
+
 
 export const Badges = () => {
    
@@ -15,19 +73,19 @@ export const Badges = () => {
                 <tr>
                 <td><img className ="badges" src={Boulder}></img></td>
                 
-                <td><img className ="badges" src="https://archives.bulbagarden.net/media/upload/thumb/9/9c/Cascade_Badge.png/900px-Cascade_Badge.png"></img></td>
+                <td><img className ="badges" src={Cascade}></img></td>
                 
-                <td><img className ="badges" src="https://www.pikpng.com/pngl/b/340-3405650_pokemon-badge-png.png"></img></td>
+                <td><img className ="badges" src={Thunder}></img></td>
                 
-                <td><img className ="badges" src="https://i1.wp.com/press.invincible.ink/wp-content/uploads/2019/01/badges_06_rainbow-300x300.png?ssl=1"></img></td>
+                <td><img className ="badges" src={Rainbow}></img></td>
                 
-                <td><img className ="badges" src="https://archives.bulbagarden.net/media/upload/7/7d/Soul_Badge.png"></img></td>
+                <td><img className ="badges" src={Soul}></img></td>
                 
-                <td><img className ="badges" src="https://cdn2.bulbagarden.net/upload/thumb/6/6b/Marsh_Badge.png/600px-Marsh_Badge.png"></img></td>
+                <td><img className ="badges" src={Marsh}></img></td>
             
-                <td><img className ="badges" src="https://webstockreview.net/images/pokemon-clipart-badges-3.png"></img></td>
+                <td><img className ="badges" src={Volcano}></img></td>
                 
-                <td><img className ="badges" src="https://vignette.wikia.nocookie.net/pokemonlp-utomaikeru/images/7/78/Earth_Badge.png/revision/latest?cb=20160316043150"></img></td>
+                <td><img className ="badges" src={Earth}></img></td>
                 </tr>
             </tr>
         </table>
@@ -38,21 +96,21 @@ export const Badges = () => {
             </tr>
             <tr bgcolor="lightgray">
                 <tr>
-                <td><img className ="badges" src="https://stegriff.co.uk/assets/old-content/badges/files/GSC/1zephyr.png"></img></td>
+                <td><img className ="badges" src={Zephyr}></img></td>
                 
-                <td><img className ="badges" src="https://archives.bulbagarden.net/media/upload/0/08/Hive_Badge.png"></img></td>
+                <td><img className ="badges" src={Hive}></img></td>
                 
-                <td><img className ="badges" src="https://archives.bulbagarden.net/media/upload/a/a7/Plain_Badge.png"></img></td>
+                <td><img className ="badges" src={Plain}></img></td>
                 
-                <td><img className ="badges" src="https://archives.bulbagarden.net/media/upload/4/48/Fog_Badge.png"></img></td>
+                <td><img className ="badges" src={Fog}></img></td>
                 
-                <td><img className ="badges" src="https://archives.bulbagarden.net/media/upload/b/b9/Storm_Badge.png"></img></td>
+                <td><img className ="badges" src={Storm}></img></td>
                 
-                <td><img className ="badges" src="https://archives.bulbagarden.net/media/upload/7/7b/Mineral_Badge.png"></img></td>
+                <td><img className ="badges" src={Mineral}></img></td>
 
-                <td><img className ="badges" src="https://archives.bulbagarden.net/media/upload/e/e6/Glacier_Badge.png"></img></td>
+                <td><img className ="badges" src={Glacier}></img></td>
                 
-                <td><img className ="badges" src="https://archives.bulbagarden.net/media/upload/5/58/Rising_Badge.png"></img></td>
+                <td><img className ="badges" src={Rising}></img></td>
                 </tr>
             </tr>
         </table>
@@ -63,21 +121,21 @@ export const Badges = () => {
             </tr>
             <tr bgcolor="lightgray">
                 <tr>
-                <td><img className ="badges" src="https://archives.bulbagarden.net/media/upload/6/63/Stone_Badge.png"></img></td>
+                <td><img className ="badges" src={Stone}></img></td>
                 
-                <td><img className ="badges" src="http://img3.wikia.nocookie.net/__cb20140715005831/pokemon/images/b/b4/Knuckle_badge.png"></img></td>
+                <td><img className ="badges" src={Knuckle}></img></td>
                 
-                <td><img className ="badges" src="http://img3.wikia.nocookie.net/__cb20140715010553/pokemon/images/b/b4/Dynamo_badge.png"></img></td>
+                <td><img className ="badges" src={Dynamo}></img></td>
                 
-                <td><img className ="badges" src="https://archives.bulbagarden.net/media/upload/c/c4/Heat_Badge.png"></img></td>
+                <td><img className ="badges" src={Heat}></img></td>
                 
-                <td><img className ="badges" src="http://img3.wikia.nocookie.net/__cb20140715173933/pokemon/images/a/af/Balance_badge.png"></img></td>
+                <td><img className ="badges" src={Balance}></img></td>
                 
-                <td><img className ="badges" src="https://archives.bulbagarden.net/media/upload/6/62/Feather_Badge.png"></img></td>
+                <td><img className ="badges" src={Feather}></img></td>
 
-                <td><img className ="badges" src="https://archives.bulbagarden.net/media/upload/c/cc/Mind_Badge.png"></img></td>
+                <td><img className ="badges" src={Mind}></img></td>
                 
-                <td><img className ="badges" src="https://2img.net/h/archives.bulbagarden.net/media/upload/9/9b/Rain_Badge.png"></img></td>
+                <td><img className ="badges" src={Rain}></img></td>
                 </tr>
             </tr>
         </table>
@@ -88,21 +146,21 @@ export const Badges = () => {
             </tr>
             <tr bgcolor="lightgray">
                 <tr>
-                <td><img className ="badges" src="https://archives.bulbagarden.net/media/upload/0/0b/Coal_Badge.png"></img></td>
+                <td><img className ="badges" src={Coal}></img></td>
                 
-                <td><img className ="badges" src="https://archives.bulbagarden.net/media/upload/8/8c/Forest_Badge.png"></img></td>
+                <td><img className ="badges" src={Forest}></img></td>
                 
-                <td><img className ="badges" src="https://archives.bulbagarden.net/media/upload/2/27/Cobble_Badge.png"></img></td>
+                <td><img className ="badges" src={Cobble}></img></td>
                 
-                <td><img className ="badges" src="https://archives.bulbagarden.net/media/upload/1/13/Fen_Badge.png"></img></td>
+                <td><img className ="badges" src={Fen}></img></td>
                 
-                <td><img className ="badges" src="https://archives.bulbagarden.net/media/upload/2/28/Relic_Badge.png"></img></td>
+                <td><img className ="badges" src={Relic}></img></td>
                 
-                <td><img className ="badges" src="https://cdn.staticneo.com/w/pokemon/thumb/0/00/MineBadge.png/120px-MineBadge.png"></img></td>
+                <td><img className ="badges" src={Mine}></img></td>
 
-                <td><img className ="badges" src="https://archives.bulbagarden.net/media/upload/0/09/Icicle_Badge.png"></img></td>
+                <td><img className ="badges" src={Icicle}></img></td>
                 
-                <td><img className ="badges" src="https://archives.bulbagarden.net/media/upload/0/0c/Beacon_Badge.png"></img></td>
+                <td><img className ="badges" src={Beacon}></img></td>
                 </tr>
             </tr>
         </table>
@@ -113,21 +171,21 @@ export const Badges = () => {
             </tr>
             <tr bgcolor="lightgray">
                 <tr>
-                <td><img className ="badges" src="https://archives.bulbagarden.net/media/upload/7/74/Trio_Badge.png"></img></td>
+                <td><img className ="badges" src={Trio}></img></td>
                 
-                <td><img className ="badges" src="https://archives.bulbagarden.net/media/upload/8/85/Basic_Badge.png"></img></td>
+                <td><img className ="badges" src={Basic}></img></td>
                 
-                <td><img className ="badges" src="https://archives.bulbagarden.net/media/upload/8/8a/Insect_Badge.png"></img></td>
+                <td><img className ="badges" src={Insect}></img></td>
                 
-                <td><img className ="badges" src="https://archives.bulbagarden.net/media/upload/5/5b/Bolt_Badge.png"></img></td>
+                <td><img className ="badges" src={Bolt}></img></td>
                 
-                <td><img className ="badges" src="https://archives.bulbagarden.net/media/upload/2/29/Quake_Badge.png"></img></td>
+                <td><img className ="badges" src={Quake}></img></td>
                 
-                <td><img className ="badges" src="https://archives.bulbagarden.net/media/upload/9/9c/Jet_Badge.png"></img></td>
+                <td><img className ="badges" src={Jet}></img></td>
 
-                <td><img className ="badges" src="https://archives.bulbagarden.net/media/upload/a/ac/Freeze_Badge.png"></img></td>
+                <td><img className ="badges" src={Freeze}></img></td>
                 
-                <td><img className ="badges" src="https://archives.bulbagarden.net/media/upload/c/c0/Legend_Badge.png"></img></td>
+                <td><img className ="badges" src={Legend}></img></td>
                 </tr>
             </tr>
         </table>
@@ -138,21 +196,21 @@ export const Badges = () => {
             </tr>
             <tr bgcolor="lightgray">
                 <tr>
-                <td><img className ="badges" src="https://archives.bulbagarden.net/media/upload/a/a5/Bug_Badge.png"></img></td>
+                <td><img className ="badges" src={Bug}></img></td>
                 
-                <td><img className ="badges" src="http://img2.wikia.nocookie.net/__cb20131029043723/pokemon/images/3/39/Cliff_Badge.png"></img></td>
+                <td><img className ="badges" src={Cliff}></img></td>
                 
-                <td><img className ="badges" src="https://archives.bulbagarden.net/media/upload/d/d4/Rumble_Badge.png"></img></td>
+                <td><img className ="badges" src={Rumble}></img></td>
                 
-                <td><img className ="badges" src="https://cdn2.bulbagarden.net/upload/thumb/7/7d/Plant_Badge.png/100px-Plant_Badge.png"></img></td>
+                <td><img className ="badges" src={Plant}></img></td>
                 
-                <td><img className ="badges" src="https://cdn2.bulbagarden.net/upload/thumb/f/fc/Voltage_Badge.png/100px-Voltage_Badge.png"></img></td>
+                <td><img className ="badges" src={Voltage}></img></td>
                 
-                <td><img className ="badges" src="http://vignette2.wikia.nocookie.net/pokemon/images/d/d1/Fairy_Badge.png"></img></td>
+                <td><img className ="badges" src={Fairy}></img></td>
 
-                <td><img className ="badges" src="https://onehallyu.com/uploads/trophies/Psychic_Badge_125.png.912fea7d7768578aad9a29c325401b85.png"></img></td>
+                <td><img className ="badges" src={Psychic}></img></td>
                 
-                <td><img className ="badges" src="http://img4.wikia.nocookie.net/__cb20131105162357/pokemon/images/8/84/Iceberg_Badge.png"></img></td>
+                <td><img className ="badges" src={Iceberg}></img></td>
                 </tr>
             </tr>
         </table>
@@ -163,25 +221,25 @@ export const Badges = () => {
             </tr>
             <tr bgcolor="lightgray">
                 <tr>
-                <td><img className ="badges" src="https://archives.bulbagarden.net/media/upload/0/00/Grass_Badge.png"></img></td>
+                <td><img className ="badges" src={Grass}></img></td>
                 
-                <td><img className ="badges" src="https://archives.bulbagarden.net/media/upload/7/7a/Water_Badge.png"></img></td>
+                <td><img className ="badges" src={Water}></img></td>
                 
-                <td><img className ="badges" src="https://archives.bulbagarden.net/media/upload/c/cc/Fire_Badge.png"></img></td>
+                <td><img className ="badges" src={Fire}></img></td>
                 
-                <td><img className ="badges" src="https://archives.bulbagarden.net/media/upload/2/20/Fighting_Badge.png"></img></td>
+                <td><img className ="badges" src={Fighting}></img></td>
                 
-                <td><img className ="badges" src="https://archives.bulbagarden.net/media/upload/3/30/Ghost_Badge.png"></img></td>
+                <td><img className ="badges" src={Ghost}></img></td>
                 
-                <td><img className ="badges" src="https://i2.wp.com/pokemythology.net/conteudo/galeria/insignias/galar6.png"></img></td>
+                <td><img className ="badges" src={Galar_Fairy}></img></td>
 
-                <td><img className ="badges" src="https://cdn2.bulbagarden.net/upload/thumb/3/3e/Rock_Badge.png/100px-Rock_Badge.png"></img></td>
+                <td><img className ="badges" src={Rock}></img></td>
                 
-                <td><img className ="badges" src="https://images.wikidexcdn.net/mwuploads/wikidex/5/5c/latest/20191117234308/Medalla_Hielo_EpEc.png"></img></td>
+                <td><img className ="badges" src={Ice}></img></td>
 
-                <td><img className ="badges" src="https://images.wikidexcdn.net/mwuploads/wikidex/b/b0/latest/20191117234310/Medalla_Siniestro_EpEc.png"></img></td>
+                <td><img className ="badges" src={Dark}></img></td>
 
-                <td><img className ="badges" src="https://images.wikidexcdn.net/mwuploads/wikidex/3/31/latest/20191117234312/Medalla_Drag%C3%B3n_EpEc.png"></img></td>
+                <td><img className ="badges" src={Dragon}></img></td>
                 </tr>
             </tr>
         </table>
