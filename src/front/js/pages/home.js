423cd1ebcfb20862cd28dfe594c8a9192e00b7a9
Bennyno <ben.fernandez28@gmail.com>
2023-01-26 16:36:52 +0000

    Update Use Effect

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -6,16 +6,16 @@ import "../../styles/home.css";
 
 export const Home = () => {
   const { store, actions } = useContext(Context);
-  const [ render, rerender] = useState(true)
+  const [render, rerender] = useState(true);
 
-  // useEffect(() => {
-  //   actions.getPokemon();
-  // }, []);
+  useEffect(() => {
+    actions.getPokemon();
+  }, []);
 
   const load_pokemon = () => {
     actions.getPokemon();
     rerender(!render);
-  }
+  };
 
   return (
     <>
@@ -23,53 +23,50 @@ export const Home = () => {
         <strong>Pokedex</strong>
       </h1>
       {store.token && store.token != "" && store.token != undefined ? (
-      <InfiniteScroll
-        dataLength={store.pokemon.length} 
-        next={load_pokemon}
-        hasMore={store.pokemon.length < 905}
-        loader={<h4>Loading...</h4>}
+        <InfiniteScroll
+          dataLength={store.pokemon.length}
+          next={load_pokemon}
+          hasMore={store.pokemon.length < 905}
+          loader={<h4>Loading...</h4>}
         >
-        <div className="card">
-        {store.pokemon.map((pokemon, index) => {
-          
-          return (
-            <PokemonCard
-              key={index}
-              image={pokemon.image}
-              name={pokemon.name}
-              type={pokemon.type}
-              flavor_text={pokemon.flavor_text}
-              index={index}
-            />
-          );
-        })}
-      </div>
-      </InfiniteScroll>
+          <div className="card">
+            {store.pokemon.map((pokemon, index) => {
+              return (
+                <PokemonCard
+                  key={index}
+                  image={pokemon.image}
+                  name={pokemon.name}
+                  type={pokemon.type}
+                  flavor_text={pokemon.flavor_text}
+                  index={index}
+                />
+              );
+            })}
+          </div>
+        </InfiniteScroll>
       ) : (
         <InfiniteScroll
-        dataLength={store.pokemon.length} 
-        next={load_pokemon}
-        hasMore={store.pokemon.length < 100}
-        loader={<h4>Loading...</h4>}
+          dataLength={store.pokemon.length}
+          next={load_pokemon}
+          hasMore={store.pokemon.length < 100}
+          loader={<h4>Loading...</h4>}
         >
-        <div className="card">
-        {store.pokemon.map((pokemon, index) => {
-          
-          return (
-            <PokemonCard
-              key={index}
-              image={pokemon.image}
-              name={pokemon.name}
-              type={pokemon.type}
-              flavor_text={pokemon.flavor_text}
-              index={index}
-            />
-          );
-        })}
-      </div>
-      </InfiniteScroll>
-      )
-      }
+          <div className="card">
+            {store.pokemon.map((pokemon, index) => {
+              return (
+                <PokemonCard
+                  key={index}
+                  image={pokemon.image}
+                  name={pokemon.name}
+                  type={pokemon.type}
+                  flavor_text={pokemon.flavor_text}
+                  index={index}
+                />
+              );
+            })}
+          </div>
+        </InfiniteScroll>
+      )}
     </>
   );
 };
